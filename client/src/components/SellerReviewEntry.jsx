78ada2c5b49b0cda561ee899629ebc4d46e4f735
Josh Hertz <jhertz1@gmail.com>
2020-08-09 12:28:29 -0700

    Implemented star rating system

--- client/src/components/SellerReviewEntry.jsx
+++ client/src/components/SellerReviewEntry.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import faker from 'faker';
+import StarRatingComponent from 'react-star-rating-component';
+
 
 class SellerReviewEntry extends React.Component {
   constructor(props) {
@@ -15,22 +17,29 @@ class SellerReviewEntry extends React.Component {
 
     let reviewMap = this.props.reviews.map((review) => (
       <div key={review.id}>
-        <li>
-          {review.content}
+        <div className="seller-individual">
+          <li>
+          <StarRatingComponent
+          name="rate1"
+          starCount={5}
+          value={review.rating}
+        />
           <br></br>
-        </li>
-        <br></br>
+            {review.rating}
+            <br></br>
+            {review.reviewer}
+            <br></br>
+            {review.content}
+          </li>
+        </div>
       </div>
-
     ))
 
     return (
       <ul>
         {reviewMap}
       </ul>
-
     )
-
   }
 }
 
