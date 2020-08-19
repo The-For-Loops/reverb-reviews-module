8975b5eb84de6599dddc4703875f121cb77faba6
Josh Hertz <jhertz1@gmail.com>
2020-08-19 09:30:35 -0700

    Added a test for SellerReviews

--- client/src/tests/SellerReviews.test.js
+++ client/src/tests/SellerReviews.test.js
@@ -12,19 +12,15 @@ describe('SellerReviews', () => {
     expect(text.text()).toBe('Seller Reviews <StarRatingComponent /> (0)');
   });
 
-  it('should call the onClick function when dropdown is clicked', () => {
-    const wrapper = shallow(<SellerReviews />);
-    const mockedHandleClick = jest.fn();
-    wrapper.instance().handleClick = mockedHandleClick;
-    const dropdown = wrapper.find('div div');
-    console.log('dropdown: ', dropdown);
-    dropdown.onClick();
-    expect(mockedHandleClick).toHaveBeenCalledTimes(1);
+
+
+
+  it('should not have dropdown clicked upon page render', () => {
+    const wrapper = shallow(<SellerReviews/>);
+    expect(wrapper.state('dropClicked')).toBe(false);
   });
 
 
 
 
-
-
-})
+});
